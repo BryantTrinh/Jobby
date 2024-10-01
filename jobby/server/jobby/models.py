8b4efc70b8c4d90ecd243f7c5bd175315d6dfb38
richard-dinh <richarddinh@gmail.com>
2024-09-30 20:32:45 -0700

    update job model and make migrations

--- jobby/server/jobby/models.py
+++ jobby/server/jobby/models.py
@@ -16,7 +16,7 @@ class Job(models.Model):
     requirements = models.TextField(null=True)
     salary = models.FloatField(null=True)
     applied = models.DateField(null=True)
-    url = models.TextField(null=False, blank=False)
+    url = models.TextField(null=False, blank=False, default=None)
     state = models.ForeignKey(
         State,
         on_delete=models.CASCADE,
