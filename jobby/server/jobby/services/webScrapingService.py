b4996549ea6d4d34d33f96a07e18931df7ab8720
richard-dinh <richarddinh@gmail.com>
2024-10-10 17:07:34 -0700

    parse by indeed from joblist and details

--- jobby/server/jobby/services/webScrapingService.py
+++ jobby/server/jobby/services/webScrapingService.py
@@ -4,11 +4,7 @@ import google.generativeai as genai
 import os          
 from dotenv import load_dotenv
 from pathlib import Path
-from bs4 import BeautifulSoup
-import time
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-import json
+from scrapeUtils import linkToSoup_selenium, parseJson
 
 # Load env file from jobby/server/server/.env
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -29,7 +25,6 @@ def parseWebsite(url):
         "www.indeed.com",
         "www.linkedin.com"
     ])
-
     urlComponents = urlparse(url)
     print(urlComponents.netloc)
 
@@ -38,34 +33,46 @@ def parseWebsite(url):
         print('Error getting soup')
         return
     
+    queryDict = parse_qs(urlComponents.query)
     if urlComponents.netloc in websiteSet:
         if urlComponents.netloc == 'www.indeed.com':
-            parseIndeed(url, urlComponents, soup)
+            parseIndeed(url, queryDict, soup)
         elif urlComponents.netloc == 'www.linkedin.com':
-            parseLinkedIn(url, urlComponents)
+            parseLinkedIn(url, queryDict)
     # else:
-        # Do other parsing
+    #     Do other parsing
 
 
-def parseIndeed(url, urlComponents, soup):
-
-    jobData = soup.find(id='jobsearch-ViewjobPaneWrapper')
+def parseIndeed(url, queryDict, soup):
     # print(jobData)
+    htmlText = None
+    if 'vjk' in queryDict:
+        # parse from jobList
+        jobData = soup.find(id='jobsearch-ViewjobPaneWrapper')
+        htmlText = jobData.text
+        
+    else:
+        # parse from job details
+        jobInfo = soup.find('div', class_='jobsearch-InfoHeaderContainer')
+        jobDetails = soup.find(id='jobDetailsSection')
+        jobLocation = soup.find(id='jobDetailsSection')
+        jobDescription = soup.find(id='jobDescriptionText')
 
-    query = "Create an object with the following fields extracted from the data: job_title as string, company as string, job_description as string, job_location as string, salary as string, job_requirements as array of strings from the HTML element " + jobData.text 
+        htmlText = jobInfo.text + jobDetails.text + jobLocation.text + jobDescription.text
 
-    apiResponse = llm_function(query);
-    jobDict = parseJson(apiResponse.candidates[0].content.parts[0].text)
-    print(jobDict)
-    return jobDict
+    if htmlText is not None:
+        query = "Create a json object with the following fields extracted from the data: job_title as string, company as string, job_description (including any responsibilities) as string, job_location as string, salary as string, job_requirements as array of strings from the HTML element " + htmlText
+        apiResponse = llm_function(query);
+        jobDict = parseJson(apiResponse.candidates[0].content.parts[0].text)
+        return jobDict
+    # else:
+    #     return None
 
 
 
-def parseLinkedIn(url, urlComponents):
+def parseLinkedIn(url, queryDict):
     print('parse linkedin site')
-    print(urlComponents)
 
-    queryDict = parse_qs(urlComponents.query)
     print(queryDict)
     if 'currentJobId' in queryDict:
         print('parse from linkedin job list')
@@ -76,44 +83,15 @@ def llm_function(query):
     response = model.generate_content(query);
     return response
 
-
-
-# helper functions
-def linkToSoup_selenium(lUrl, tmout=None, fparser='html.parser', isv=True, returnErr=False):
-    try:
-        # I copy chromedriver.exe to the same folder as this py file
-        options = ChromeOptions()
-        options.add_argument("--headless=old")
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.90 Safari/537.36'
-        options.add_argument(f'user-agent={user_agent}')
-        driver = webdriver.Chrome(options=options)
-        driver.maximize_window()
-        driver.get(lUrl)
-        if type(tmout) in [int, float]: time.sleep(tmout) 
- 
-        lSoup = BeautifulSoup(driver.page_source, fparser)
-        driver.close()
-        del driver  # (just in case)
-        return lSoup
-    except Exception as e:
-        if isv: print(str(e))   ## set isv=False to suppress error message ##
-        return str(e) if returnErr else None
-
-
-def parseJson(text):
-    # clean u ` chars from response
-    removedChars = text.replace('`', '')
-    try:
-        json_object = json.loads(removedChars)
-        return json_object
-    except json.JSONDecodeError:
-        print("Invalid Json Format")
 # for testing
 if __name__ == '__main__':
     
     print(GOOGLE_API_KEY)
+    # parse from job list
     parseWebsite('https://www.indeed.com/jobs?q=software+engineer&l=Remote&sc=0kf%3Aattr%28EVPJU%7CJB2WC%7CNGEEK%7CWD7PP%252COR%29%3B&from=searchOnDesktopSerp&vjk=dcd58daeb24cdbda&advn=1237621172012789')
 
+    # parse from job details
+    parseWebsite('https://www.indeed.com/viewjob?jk=27d9d63f723eccc8&q=software+engineer&l=Remote&tk=1i9sad68bi023816&from=web&advn=9656130729393009&adid=437268302&ad=-6NYlbfkN0DgaN-HpXIqjvu5EzBtjgqLFI86fHlVkW4KWJhOZyoyA8VV2iWf7cPiDeWS_G-FQ74gilsZEzbPkGGgdR_Iqwc6Axlu2C_-eDlDk8_3doE036IYhgrdH-Kv74myxfK_blOdL5ENoxMp8pVa7qJYwNUhBOjvaofCduU9NMixhKZ9DiQicVWZNtt55NgyxAmvN0DaqI1WghKvbWXyPqRj_GExenjMqh40H4PaJ8wI2NYKOsj3S2Ie_oA4f3DCgiMGL9rKsWzFH_El3XhrpvJodM8mAbj8xrhnqT6C-c_KsBCAS_QUqxiimwLzou4VmXwpUmafLJRMNhG9qm8gyNVBMOAkidmWR6hE2jiHJE6tm7ihumSnj2E_wVhCqG0VMFVNEYrUXLMb9QugBN7ZguQYO8bFR8njM2PYj3qaRrmtFcpJjyayy7GzV_GXlrk2tPg3TpSv5M0h9BqhvsQgGrcU7t8axs93-FtTf4uyagpoVD4QFOiG5sklGiWPaoJAnTwNXrwX1F_TJ-CfNKdehXqdL60v&pub=4a1b367933fd867b19b072952f68dceb&camk=nUmJqO2E8rjNfblg4Prjlw%3D%3D&xkcb=SoBy6_M378c7FKTxYR0LbzkdCdPP&xpse=SoBZ6_I378c5nxzxaR0PbzkdCdPP&xfps=98a845cd-954e-44a6-8594-4978ece3ea5f&vjs=3')
     # parseWebsite('https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4041180884')
 
     # parseWebsite('https://www.linkedin.com/jobs/view/4041180884/?alternateChannel=search&refId=a9aDr%2FE8Jzz%2BiAjzfkIrYA%3D%3D&trackingId=84J8W82CAexB5l%2BUsz1TTA%3D%3D')
