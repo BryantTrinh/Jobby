fd1485c7f67caf94162f4f8f2721758bb754e831
richard-dinh <richarddinh@gmail.com>
2024-12-12 09:39:37 -0800

    open linkedIn browser using user profile

--- jobby/server/jobby/services/scrapeUtils.py
+++ jobby/server/jobby/services/scrapeUtils.py
@@ -1,10 +1,14 @@
 
+import os    
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from urllib.parse import urlparse
 import json
 
+USERNAME = os.getenv('USERNAME')
 # given a url, convert link to soup Beautiful soup using selenium
 def linkToSoup_selenium(lUrl, tmout=None, fparser='html.parser', isv=True, returnErr=False):
     try:
@@ -25,6 +29,38 @@ def linkToSoup_selenium(lUrl, tmout=None, fparser='html.parser', isv=True, retur
     except Exception as e:
         if isv: print(str(e))   ## set isv=False to suppress error message ##
         return str(e) if returnErr else None
+    
+def linkToSoup_selenium_linkedIn(lUrl, tmout=None, fparser='html.parser', isv=True, returnErr=False):
+    try:
+        # I copy chromedriver.exe to the same folder as this py file
+        options = ChromeOptions()
+        # options.add_argument("--headless=old")
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.90 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
+
+        # HAVE TO CREATE A NEW COPY OF USER DATA FOLDER WHERE CHROME USER DATA IS STORED. IN THISE CASE, CREATED A COPY IN SAME DIRECTORY AND NAMED IT JOBBY. MUST BE LOGGED INTO LINKEDIN ON SECOND PRFILE
+        options.add_argument("--user-data-dir=C:\\Users\\Richard\\AppData\\Local\\Google\\Chrome\\Jobby")
+        # options.add_argument("user-data-dir=C:\\Users\\Richard\\AppData\\Local\\Google\\Chrome\\User Data")
+        # options.add_argument("--user-data-dir=C:\\Users\\Richard\\Documents\\Chrome Profile")
+        options.add_argument('--profile-directory=Profile 2')
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-infobars")
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        driver.get(lUrl)
+        if type(tmout) in [int, float]: time.sleep(tmout) 
+    
+        lSoup = BeautifulSoup(driver.page_source, fparser)
+        driver.close()
+        del driver  # (just in case)
+        return lSoup
+
+    except Exception as e:
+        if isv: print(str(e))   ## set isv=False to suppress error message ##
+        return str(e) if returnErr else None
 
 
 # Given text from Google gemini, remove all unneed chars and convert object to json
@@ -37,3 +73,18 @@ def parseJson(text):
         return json_object
     except json.JSONDecodeError:
         print("Invalid Json Format")
+
+
+def handle_login(redirect_url, driver, fparser='html.parser'):
+    driver.get(redirect_url)
+    time.sleep(15)
+    
+    loginSoup = BeautifulSoup(driver.page_source, fparser)
+
+    print(loginSoup)
+
+    # authwall-join-form__form-toggle--bottom form-toggle
+    # driver.find_element_by_css_selector('.authwall-join-form__form-toggle--bottom.form-toggle').click()
+    driver.find_elements(By.CLASS_NAME, '.nav__button-secondary btn-secondary-emphasis.btn-md').click()
+    # driver.find_elements(By.CLASS_NAME, '.authwall-join-form__form-toggle--bottom.form-toggle').click()
+    time.sleep(30)
--- jobby/server/jobby/services/webScrapingService.py
+++ jobby/server/jobby/services/webScrapingService.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 import os          
 from dotenv import load_dotenv
 from pathlib import Path
-from .scrapeUtils import linkToSoup_selenium, parseJson
+from .scrapeUtils import linkToSoup_selenium, parseJson, linkToSoup_selenium_linkedIn
 
 # Load env file from jobby/server/server/.env
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -28,18 +28,25 @@ def parse_website(url):
     urlComponents = urlparse(url)
     print(urlComponents.netloc)
 
-    soup = linkToSoup_selenium(url, isv=True, returnErr=False)
-    if not soup:
-        print('Error getting soup')
-        return
+    soup = None
     
+    print(soup)
     queryDict = parse_qs(urlComponents.query)
     jobDict = None
     if urlComponents.netloc in websiteSet:
         if urlComponents.netloc == 'www.indeed.com':
+            soup = linkToSoup_selenium(url, isv=True, returnErr=False)
+            if not soup:
+                print('Error getting soup')
+                return
             jobDict = parse_indeed(url, queryDict, soup)
         elif urlComponents.netloc == 'www.linkedin.com':
-            jobDict = parse_linkedIn(url, queryDict)
+            soup = linkToSoup_selenium_linkedIn(url, tmout = 8, isv=True, returnErr=False)
+            if not soup:
+                print('Error getting soup')
+                return
+            print(soup)
+            # jobDict = parse_linkedIn(url, queryDict, soup)
     # else:
     #     Do other parsing
     return jobDict
@@ -74,14 +81,20 @@ def parse_indeed(url, queryDict, soup):
 
 
 
-def parse_linkedIn(url, queryDict):
+def parse_linkedIn(url, queryDict, soup):
     print('parse linkedin site')
 
-    print(queryDict)
-    if 'currentJobId' in queryDict:
-        print('parse from linkedin job list')
-    else:
-        print('parse from linked job details')
+    # print(queryDict)
+
+    jobData = soup.select('.details')
+    # jobData = soup.select('.job-view-layout.jobs-details')
+
+    print(jobData)
+
+    # if 'currentJobId' in queryDict:
+    #     print('parse from linkedin job list')
+    # else:
+    #     print('parse from linked job details')
 
 def llm_function(query):
     response = model.generate_content(query);
@@ -90,14 +103,13 @@ def llm_function(query):
 # for testing
 # if __name__ == '__main__':
 
-#     for line in sys.path:
-#         print(line)
+
 #     print(GOOGLE_API_KEY)
-#     # # parse from job list
-#     # parse_website('https://www.indeed.com/jobs?q=software+engineer&l=Remote&sc=0kf%3Aattr%28EVPJU%7CJB2WC%7CNGEEK%7CWD7PP%252COR%29%3B&from=searchOnDesktopSerp&vjk=dcd58daeb24cdbda&advn=1237621172012789')
+    # # parse from job list
+    # parse_website('https://www.indeed.com/jobs?q=software+engineer&l=Remote&sc=0kf%3Aattr%28EVPJU%7CJB2WC%7CNGEEK%7CWD7PP%252COR%29%3B&from=searchOnDesktopSerp&vjk=dcd58daeb24cdbda&advn=1237621172012789')
 
-#     # # parse from job details
-#     # parse_website('https://www.indeed.com/viewjob?jk=27d9d63f723eccc8&q=software+engineer&l=Remote&tk=1i9sad68bi023816&from=web&advn=9656130729393009&adid=437268302&ad=-6NYlbfkN0DgaN-HpXIqjvu5EzBtjgqLFI86fHlVkW4KWJhOZyoyA8VV2iWf7cPiDeWS_G-FQ74gilsZEzbPkGGgdR_Iqwc6Axlu2C_-eDlDk8_3doE036IYhgrdH-Kv74myxfK_blOdL5ENoxMp8pVa7qJYwNUhBOjvaofCduU9NMixhKZ9DiQicVWZNtt55NgyxAmvN0DaqI1WghKvbWXyPqRj_GExenjMqh40H4PaJ8wI2NYKOsj3S2Ie_oA4f3DCgiMGL9rKsWzFH_El3XhrpvJodM8mAbj8xrhnqT6C-c_KsBCAS_QUqxiimwLzou4VmXwpUmafLJRMNhG9qm8gyNVBMOAkidmWR6hE2jiHJE6tm7ihumSnj2E_wVhCqG0VMFVNEYrUXLMb9QugBN7ZguQYO8bFR8njM2PYj3qaRrmtFcpJjyayy7GzV_GXlrk2tPg3TpSv5M0h9BqhvsQgGrcU7t8axs93-FtTf4uyagpoVD4QFOiG5sklGiWPaoJAnTwNXrwX1F_TJ-CfNKdehXqdL60v&pub=4a1b367933fd867b19b072952f68dceb&camk=nUmJqO2E8rjNfblg4Prjlw%3D%3D&xkcb=SoBy6_M378c7FKTxYR0LbzkdCdPP&xpse=SoBZ6_I378c5nxzxaR0PbzkdCdPP&xfps=98a845cd-954e-44a6-8594-4978ece3ea5f&vjs=3')
-#     # parse_website('https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4041180884')
+    # # parse from job details
+    # parse_website('https://www.indeed.com/viewjob?jk=27d9d63f723eccc8&q=software+engineer&l=Remote&tk=1i9sad68bi023816&from=web&advn=9656130729393009&adid=437268302&ad=-6NYlbfkN0DgaN-HpXIqjvu5EzBtjgqLFI86fHlVkW4KWJhOZyoyA8VV2iWf7cPiDeWS_G-FQ74gilsZEzbPkGGgdR_Iqwc6Axlu2C_-eDlDk8_3doE036IYhgrdH-Kv74myxfK_blOdL5ENoxMp8pVa7qJYwNUhBOjvaofCduU9NMixhKZ9DiQicVWZNtt55NgyxAmvN0DaqI1WghKvbWXyPqRj_GExenjMqh40H4PaJ8wI2NYKOsj3S2Ie_oA4f3DCgiMGL9rKsWzFH_El3XhrpvJodM8mAbj8xrhnqT6C-c_KsBCAS_QUqxiimwLzou4VmXwpUmafLJRMNhG9qm8gyNVBMOAkidmWR6hE2jiHJE6tm7ihumSnj2E_wVhCqG0VMFVNEYrUXLMb9QugBN7ZguQYO8bFR8njM2PYj3qaRrmtFcpJjyayy7GzV_GXlrk2tPg3TpSv5M0h9BqhvsQgGrcU7t8axs93-FtTf4uyagpoVD4QFOiG5sklGiWPaoJAnTwNXrwX1F_TJ-CfNKdehXqdL60v&pub=4a1b367933fd867b19b072952f68dceb&camk=nUmJqO2E8rjNfblg4Prjlw%3D%3D&xkcb=SoBy6_M378c7FKTxYR0LbzkdCdPP&xpse=SoBZ6_I378c5nxzxaR0PbzkdCdPP&xfps=98a845cd-954e-44a6-8594-4978ece3ea5f&vjs=3')
+    # parse_website('https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4041180884')
 
-#     # parse_website('https://www.linkedin.com/jobs/view/4041180884/?alternateChannel=search&refId=a9aDr%2FE8Jzz%2BiAjzfkIrYA%3D%3D&trackingId=84J8W82CAexB5l%2BUsz1TTA%3D%3D')
+    # parse_website('https://www.linkedin.com/jobs/view/4041180884/?alternateChannel=search&refId=a9aDr%2FE8Jzz%2BiAjzfkIrYA%3D%3D&trackingId=84J8W82CAexB5l%2BUsz1TTA%3D%3D')
